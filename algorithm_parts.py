8609a2f88607db63544a1fa865ae2d9835500277
Charlie Batchelor <charliebatchelor1@hotmail.co.uk>
2023-02-23 10:03:04 +0000

    Added functionality to process DAPHNE data waveforms.

--- algorithm_parts.py
+++ algorithm_parts.py
@@ -1,3 +1,13 @@
+# Simple hit class - has all the information to generate hits to play in DUNE DAQ
+class Hit:
+    def __init__(self, channel, start_time, peak_charge, sum_charge, time_over_threshold):
+        self.channel = 0
+        self.start_time = 0
+        self.peak_charge = 0
+        self.sum_charge = 0
+        self.time_over_threshold = 0
+
+
 # Find and return the frugal pedestal of the raw waveform. This corresponds to
 # an adapting pedestal along the waveform, of size waveform.size
 def frugal_pedestal(waveform, ncontig):
@@ -44,4 +54,72 @@ def apply_fir_filter(pedsub, ntaps, taps):
     return filtered
 
 
+# Returns a list/array down sampled representation of the original waveform
+def down_sample(waveform, down_sample_factor):
+
+    # Just return the original waveform for unit down_sample_factor
+    if down_sample_factor == 1:
+        return waveform
+
+    # Otherwise, return every down_sample_factor-th element of the original waveform
+    down_sampled_waveform = []
+    for i in waveform[::down_sample_factor]:
+        down_sampled_waveform.append(i)
+    return down_sampled_waveform
+
+
+# Returns a list/array of median values corresponding to a running baseline/pedestal
+def find_pedestal(waveform, use_sigkill, frugal_ped_ncontig):
+    if use_sigkill:
+        ped = frugal_pedestal_sigkill(waveform)
+    else:
+        ped = frugal_pedestal(waveform, frugal_ped_ncontig)
+    return ped
+
+
+# Filter
+def filter(pedsub, filter_taps, do_filtering):
+    ntaps = len(filter_taps)
+    taps = filter_taps
+
+    # If we are doing filter, apply FIR (finite impulse response) filter:
+    if do_filtering:
+        return apply_fir_filter(pedsub, ntaps, taps)
+    else:
+        return pedsub
+
+
+# Does the hit finding for specified channel and returns list of hits
+def hit_finding(waveform, channel, down_sample_factor, threshold):
+    is_hit = False
+    was_hit = False
+    hit = Hit(channel, 0, 0, 0, 0)
+    hit_charge = []
+    hits = []
+
+    for i, s in enumerate(waveform):
+        sample_time = i * down_sample_factor
+        adc = s
+
+        # Introduce ignorance of first ~100 ticks for pedestal to stabalise
+        is_hit = adc > threshold
+        if is_hit and not was_hit:
+            # Starting a hit, set start time:
+            hit_charge.append(adc)
+            hit.start_time = sample_time
+            hit.sum_charge = adc
+            hit.time_over_threshold = down_sample_factor
+        if is_hit and was_hit:
+            # Continuing a hit, add stuff
+            hit_charge.append(adc)
+            hit.sum_charge += adc * down_sample_factor
+            hit.time_over_threshold += down_sample_factor
+        if not is_hit and was_hit:
+            # The hit is finished, output it.
+            hit.peak_charge = max(hit_charge)
+            hits.append(hit)
+            hit_charge.clear()
+        was_hit = is_hit
+
+    return hits
 
--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from algorithm_parts import *
 from basic_simulation_testing import *
+import json
 
 # Parameters ------------------------------------------------------
-threshold = 5  # Hit finding threshold on ADC
-down_sample_factor = 1  # Factor by which to reduce the information in waveform
-frugal_ped_ncontig = 5  # Number of samples to consider before adjusting median in ped finding
-use_sigkill = False  # Implement this later
-do_filtering = True  # Boolean, for deciding whether we do filtering
+threshold = 100           # Hit finding threshold on ADC
+down_sample_factor = 1    # Factor by which to reduce the information in waveform
+frugal_ped_ncontig = 5    # Number of samples to consider before adjusting median in ped finding
+use_sigkill = False       # Implement this later
+do_filtering = True       # Boolean, for deciding whether we do filter
 filter_taps = [1, 3, 6, 9, 6, 3, 1]  # FIR Taps - Need tuning
 
 # Basic simulation testing ----------------------------------------
-n = 500                       # Scale/size of simulated waveform
+n = 500                         # Scale/size of simulated waveform
 x = np.linspace(0, n, n)        # Default size of sim waveform
 n_waveforms = 10                # Default number of waveforms to make
-use_simulation = True           # Only doing simulation for now
+use_simulation = False          # Only doing simulation for now
 plotting_waveform = 0           # Test waveform to plot relevant data
-use_random_walk = True        # It's random walk or noisy sine wave
+use_random_walk = True          # It's random walk or noisy sine wave
+
+# Data testing ----------------------------------------------------
+use_data = True
+num_forms = 2                   # Number of waveforms to run hit finding on
+
+# Plotting parameters ---------------------------------------------
+plotting = True
 
 
 # Algorithm functions ---------------------------------------------
@@ -33,101 +41,21 @@ def print_config():
     print()
 
 
-# Returns a list/array down sampled representation of the original waveform
-def down_sample(waveform):
-
-    # Just return the original waveform for unit down_sample_factor
-    if down_sample_factor == 1:
-        return waveform
-
-    # Otherwise, return every down_sample_factor-th element of the original waveform
-    down_sampled_waveform = []
-    for i in waveform[::down_sample_factor]:
-        down_sampled_waveform.append(i)
-    return down_sampled_waveform
-
-
-# Returns a list/array of median values corresponding to a running baseline/pedestal
-def find_pedestal(waveform):
-    if use_sigkill:
-        ped = frugal_pedestal_sigkill(waveform)
-    else:
-        ped = frugal_pedestal(waveform, frugal_ped_ncontig)
-    return ped
-
-
-# Filter
-def filter(pedsub):
-    ntaps = len(filter_taps)
-    taps = filter_taps
-
-    # If we are doing filter, apply FIR (finite impulse response) filter:
-    if do_filtering:
-        return apply_fir_filter(pedsub, ntaps, taps)
-    else:
-        return pedsub
-
-
-# Simple hit class - has all the information to generate hits to play in DUNE DAQ
-class Hit:
-    def __init__(self, channel, start_time, peak_charge, sum_charge, time_over_threshold):
-        self.channel = 0
-        self.start_time = 0
-        self.peak_charge = 0
-        self.sum_charge = 0
-        self.time_over_threshold = 0
-
-
-# Does the hit finding for specified channel and returns list of hits
-def hit_finding(waveform, channel):
-    is_hit = False
-    was_hit = False
-    hit = Hit(channel, 0, 0, 0, 0)
-    hit_charge = []
-    hits = []
-
-    for i, s in enumerate(waveform):
-        sample_time = i * down_sample_factor
-        adc = s
-
-        # Introduce ignorance of first ~100 ticks for pedestal to stabalise
-        is_hit = adc > threshold
-        if is_hit and not was_hit:
-            # Starting a hit, set start time:
-            hit_charge.append(adc)
-            hit.start_time = sample_time
-            hit.sum_charge = adc
-            hit.time_over_threshold = down_sample_factor
-        if is_hit and was_hit:
-            # Continuing a hit, add stuff
-            hit_charge.append(adc)
-            hit.sum_charge += adc * down_sample_factor
-            hit.time_over_threshold += down_sample_factor
-        if not is_hit and was_hit:
-            # The hit is finished, output it.
-            hit.peak_charge = max(hit_charge)
-            hits.append(hit)
-            hit_charge.clear()
-        was_hit = is_hit
-
-    return hits
-
-
 # Supply a list of channels and a list of waveforms to find hits on
 def find_hits(channels, samples):
     hits = []  # The creature we return
-    print("Called 'find_hits()' with ", len(samples), " channels. First channel has ", len(samples[0]), " samples.")
+    print("\nCalled 'find_hits()' with ", len(samples), " channels. First channel has ", len(samples[0]), " samples.")
 
     for i, s in enumerate(samples):
         og_waveform = s
-        waveform = down_sample(og_waveform)
-        pedestal = find_pedestal(waveform)
+        waveform = down_sample(og_waveform, down_sample_factor)
+        pedestal = find_pedestal(waveform, use_sigkill, frugal_ped_ncontig)
         pedsub = []
         for j, adc in enumerate(waveform):
             pedsub.append(adc - pedestal[j])
 
-        filtered = filter(pedsub)
-        hits.append(hit_finding(filtered, channels[i]))
+        filtered = filter(pedsub, filter_taps, do_filtering)
+        hits.append(hit_finding(filtered, channels[i], down_sample_factor, threshold))
 
     print("Hit finding complete, returning ", len(hits[0]), " hits for first channel.")
     print("Returning total of ", len(hits), " lists of hits, should be one for each channel.")
@@ -141,7 +69,6 @@ if __name__ == '__main__':
 
     # SIMULATED GENERIC WAVEFORMS =======================================
     if use_simulation:
-        # Test waveform
         test_waveforms = []
         channels = []
         for i in range(0, n_waveforms):
@@ -149,24 +76,61 @@ if __name__ == '__main__':
                 test_waveforms.append(sim_waveform(x))
             else:
                 test_waveforms.append(sin_waveform(x))
-            channels.append(i)
+            channels.append(i+1)
 
+        # The heavy lifting is done here, find the hits.
         hits = find_hits(channels, test_waveforms)
-    # === TESTING Complete - Now plot a sample waveform and data ========
-        # Get ped sub for sample waveform
+
+        # Hit Finding Complete - Now plot a sample waveform and data =====
+        # Do pedestal subtraction for sample waveform
         tw = test_waveforms[plotting_waveform]
-        ped = find_pedestal(tw)
+        ped = find_pedestal(tw, use_sigkill, frugal_ped_ncontig)
         pedsub = []
         for j, adc in enumerate(tw):
             pedsub.append(adc - ped[j])
 
         plt.plot(tw, label="Raw Simulated Waveform")
-        plt.plot(find_pedestal(tw), label="Found Pedestal - Raw")
+        plt.plot(find_pedestal(tw, use_sigkill, frugal_ped_ncontig), label="Found Pedestal")
         plt.plot(pedsub, label="Pedestal Subtracted Waveform")
-        plt.plot(find_pedestal(pedsub), label="Found Pedestal - Ped Subtracted")
         plt.grid()
         plt.title("Simulated Waveform Example", fontweight='bold')
         plt.xlabel("Relative Time Tick", fontweight='bold')
         plt.ylabel("Arbitrary ADC Value", fontweight='bold')
         plt.legend()
         plt.show()
+
+    # Data sample - We'd like to consider real waveforms instead.
+    if use_data:
+        # Read in DAPHNE data
+        file = open('config.json')
+        config = json.load(file)
+        data_loc = config['data_location']
+        daphne_data = np.load(data_loc)
+        print("Shape of full DAPHNE dataframe: ", daphne_data.shape)
+        wfs = daphne_data[:num_forms]
+        print("Shape of single DAPHNE waveform: ", wfs[0].shape)
+        channels = [i+1 for i in range(len(wfs))]
+
+        # Do the hit finding on these waveforms separately
+        hits = find_hits(channels, wfs)
+
+        # Plot some DAPHNE data
+        if plotting:
+            # Setup subplots
+            fig, axs = plt.subplots(num_forms, 1, sharex=True, sharey=True)
+            fig.supxlabel("Relative Time - Ticks", fontweight="bold")
+            fig.supylabel("ADC Count", fontweight="bold")
+            fig.suptitle("Raw DAPHNE Waveforms - PROCESSED_CALIB_WVFS_SC.npy", fontweight="bold")
+            axs.ravel()
+
+            for i in range(0, num_forms):
+                label = "Raw Waveform: " + str(i+1)
+                axs[i].plot(wfs[i], label=label)
+                axs[i].plot(find_pedestal(wfs[i], use_sigkill, frugal_ped_ncontig), label="Pedestal")
+                axs[i].grid('both')
+                axs[i].legend()
+                axs[i].set_xlabel("Relative Time Tick", fontweight='bold')
+                axs[i].set_ylabel("Arbitrary ADC Value", fontweight='bold')
+
+            plt.show()
+
